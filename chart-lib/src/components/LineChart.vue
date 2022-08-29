94d56ffb969b90f21f0382283778e7c066a6bd91
Dong-Yeop <ehdduq53@gmail.com>
2022-08-29 21:03:14 +0900

    라인 차트 생성

---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,46 @@
+<template>
+  <canvas id="lineChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  mounted() {
+    const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ];
+
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+    
+    const myChart = new Chart(
+      document.getElementById('lineChart'),
+      config
+    );
+    console.log(myChart);
+  }
+}
+</script>
+
+<style>
+
+</style>
